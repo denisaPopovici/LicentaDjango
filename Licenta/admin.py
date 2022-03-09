de8f4b3f772d7325256f3651a8a33534555b6fba
Denisa Popovici <denisapopovici@Denisas-MacBook-Air.local>
2022-03-09 21:59:14 +0200

    feed backend

--- Licenta/admin.py
+++ Licenta/admin.py
@@ -4,3 +4,7 @@ from Licenta.models import *
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(CustomUser)
+admin.site.register(Comment)
+admin.site.register(Post)
+admin.site.register(UserLikePost)
+admin.site.register(UserFollow)
--- LicentaDjango/urls.py
+++ LicentaDjango/urls.py
@@ -9,11 +9,23 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Licenta.urls')),
+    path('api/posts', get_all_posts),
+    path('api/user/<str:userID>/feed-posts', feed_posts),
+    path('api/comments', get_all_comments),
     path('auth/', obtain_auth_token),
     path('api/locations', get_all_locations),
     path('api/<str:token>', get_user_by_token),
     path('api/user/<str:username>', get_user_by_username),
+    path('api/user-by-id/<str:userID>', change_user_image),
     path('api/custom-user/<str:username>', get_custom_user_by_username),
+    path('api/posts/<str:postID>/users/<str:userID>/like-post', like_post),
+    path('api/user/<str:userID>/liked-posts', all_posts_user_likes),
+    path('api/posts/<str:postID>/users/<str:userID>/comment-post', add_comment),
+    path('api/user/<str:userID>/post/<str:postID>/did-like', did_user_like_post),
+    path('api/user/<str:userID>/get-by-id', get_custom_user_by_id),
+    path('api/get-comments-to-post/<str:postID>', get_all_comments_to_post),
+
+
 ]
 
 if settings.DEBUG:
